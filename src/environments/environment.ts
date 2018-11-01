2a3282fb6aafdb90d9ae28a5b5fbb252707115e7
COSVC-EXT06\IMAGINAMOS <aleixer.alvarado@imaginamos.co>
2018-11-01 14:54:16 -0500

    key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   config: {
-    apiKey: 'AIzaSyBld6gp-XtjyvoSYbLlZAM6rAsOMysesCo',
-    authDomain: 'ranking-0.firebaseapp.com',
-    databaseURL: 'https://ranking-0.firebaseio.com',
-    projectId: 'ranking-0',
-    storageBucket: 'ranking-0.appspot.com',
-    messagingSenderId: '630269231233'
+    apiKey: 'sAIzaSyBld6gp-XtjysvoSYbLlZAM6rAsOMysesCos',
+    authDomain: 'firebaseapp.com',
+    databaseURL: 'https://firebaseio.com',
+    projectId: 'ranking',
+    storageBucket: 'ranking.appspot.com',
+    messagingSenderId: '630268785429231233'
   }
 };
 
